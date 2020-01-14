f3dd7c7ceee4a6f9b5c28e0aed770668c40db415
Lukasz Labusek <lukasz.labusek@gmail.com>
2020-01-14 20:34:42 +0100

    commit TreeAndHeap -added pop() to heap

--- src/pl/sda/TreeAndHeap/Heap.java
+++ src/pl/sda/TreeAndHeap/Heap.java
@@ -6,31 +6,31 @@ import java.util.List;
 
 public class Heap {
     private List<Integer> ints = new LinkedList<>(
-            Arrays.asList(12, 11, 9, 3, 5, 4)
+            Arrays.asList(12, 11, 1, 7, 5, 4)
     );
     private int i, j;
 
     private void swap() {
         int temp = ints.get(i);
         ints.set(i, ints.get(j));
-        countParentAndChildIndex();
+        i=j;
+        j=(i-1)/2;
         ints.set(i, temp);
     }
-    private void countParentAndChildIndex(){
-        i = j;
-        j = (i - 1) / 2;
-    }
+
 
     public void addElement(int elem) {
         //Przed dodaniem elementu do kopca sprawdzamy czy kopiec jest zbudowany poprawnie
         //Jezeli nie to go naprawiamy
-        makeHeapCorrectFromRoot();
+        if (!isHeapCorrectFromRoot()) makeHeapCorrectFromRoot();
         //dodajemy element do kopca
         ints.add(elem);
         //obliczamy index rodzica i dziecka
-        countParentAndChildIndex();
+        i = ints.size()-1;
+        j = (i - 1) / 2;
         //wykonujemy petle do momentu gdy element bedzie mniejszy od ojca, lub dojdziemy do konca kopca
         while (i > 0 && ints.get(j) < elem) {
+            //zamiana
             swap();
         }
         //drukujemy wynik
@@ -39,21 +39,19 @@ public class Heap {
 
     public boolean isHeapCorrectFromRoot() {
         for (int x = 1; x < ints.size(); x++) {
-            countParentAndChildIndex();
+            i = x;
+            j = (i - 1) / 2;
             if (ints.get(i) > ints.get(j)) return false;
         }
         return true;
     }
 
     private void makeHeapCorrectFromRoot() {
-        for (int x = 1; x < ints.size(); x++) {
-            int i = x - 1;
-            int j = (i - 1) / 2;
+        for (int x = 1; x <ints.size(); x++) {
+            i = x;
+            j = (i - 1) / 2;
             while (i < ints.size() && ints.get(j) < ints.get(i)) {
-                int temp = ints.get(i);
-                ints.set(i, ints.get(j));
-                countParentAndChildIndex();
-                ints.set(i, temp);
+                swap();
             }
         }
         if (!isHeapCorrectFromRoot()) makeHeapCorrectFromRoot();
